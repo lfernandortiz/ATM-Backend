a8e45e75cfb6cc6bdcc9d2ccf963ad5bc9c0c8f2
Luis Fernando Ortiz Vera <lfernandortiz@gmail.com>
2020-11-24 11:28:23 -0500

    Inicio del proyecto base de backend

---
+++ src/test/java/com/acsendo/AtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acsendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
